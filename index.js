1765004d91ea12327568419cdc2ae70a3df69d25
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-17 05:28:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+/*!
+ * is-equal-shallow <https://github.com/jonschlinkert/is-equal-shallow>
+ *
+ * Copyright (c) 2015 Jon Schlinkert.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var isPrimitive = require('is-primitive');
+
+module.exports = function isEqual(a, b) {
+  if (!a && !b) { return true; }
+  if (!a && b || a && !b) { return false; }
+
+  for (var key in b) {
+    if (!isPrimitive(b[key]) || !a.hasOwnProperty(key) || (a[key] !== b[key])) {
+      return false;
+    }
+  }
+  return true;
+};
---
+++ test.js
@@ -0,0 +1,32 @@
+/*!
+ * equals-shallow <https://github.com/jonschlinkert/equals-shallow>
+ *
+ * Copyright (c) 2015 Jon Schlinkert.
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var assert = require('assert');
+var should = require('should');
+var equals = require('./');
+
+describe('equals', function () {
+  it('should return true when keys and values are the same:', function () {
+    equals({a: true, b: true}, {a: true, b: true}).should.be.true;
+  });
+
+  it('should return false when keys are different:', function () {
+    equals({a: true, b: false}, {c: false, b: false}).should.be.false;
+  });
+
+  it('should return false when values are different:', function () {
+    equals({a: true, b: false}, {a: false, b: false}).should.be.false;
+    equals({a: true, b: true}, {a: true, b: 'true'}).should.be.false;
+  });
+
+  it('should return false when a value is not a primitive:', function () {
+    equals({ b: {}}, { b: {}}).should.be.false;
+    equals({ b: []}, { b: []}).should.be.false;
+  });
+});
