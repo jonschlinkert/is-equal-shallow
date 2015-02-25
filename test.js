74b7df801cfe1e02521715aa4ec1f91c5fca3206
jonschlinkert <jon.schlinkert@sellside.com>
2015-02-25 18:12:32 -0500

    update

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 /*!
- * equals-shallow <https://github.com/jonschlinkert/equals-shallow>
+ * is-equal-shallow <https://github.com/jonschlinkert/is-equal-shallow>
  *
- * Copyright (c) 2015 Jon Schlinkert.
- * Licensed under the MIT license.
+ * Copyright (c) 2015, Jon Schlinkert.
+ * Licensed under the MIT License.
  */
 
 'use strict';
